d906c714ff167ff98e2143216ec7e305537d8a29
lvtus <tuluong@mashallah.app>
2025-10-16 11:55:13 +0700

    refactor: Remove Total field from Stats struct
    - Eliminate the Total field from the Stats struct to streamline object metadata representation.
    - Adjust comments to reflect the updated structure of the Stats type.

--- bucket/stats.go
+++ bucket/stats.go
@@ -6,8 +6,6 @@ import "time"
 // It contains information about the total number of objects, the size of the object,
 // the content type of the object, and the last modified time of the object.
 type Stats struct {
-	// Total represents the total number of objects in the storage bucket.
-	Total int64 `json:"total" yaml:"total"`
 	// Size represents the size of the object in the storage bucket.
 	Size int64 `json:"size" yaml:"size"`
 	// ContentType represents the content type of the object in the storage bucket.
